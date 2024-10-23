Jerome226/WitchingGadgets

commit e25ca176dbc885b1c877c6df7fe1f5d5b9b730cd

Zokos Fixes

src/main/java/witchinggadgets/common/items/baubles/ItemMagicalBaubles.java
@@ -37,7 +37,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 public class ItemMagicalBaubles extends Item implements IBauble, ITravellersGear, vazkii.botania.api.item.ICosmeticAttachable
 {
 	//String[] subNames = {"ringSocketed_gold","ringSocketed_thaumium","ringSocketed_silver"};
-	public static String[] subNames = {"shouldersDoublejump","shouldersKnockback","vambraceStrength","vambraceHaste","ringLuck","ringSniper"};
+	public static String[] subNames = {"shouldersDoublejump","shouldersKnockback","vambraceStrength","vambraceHaste","ringLuck","titleCrimsonCult","ringSniper"};
 	IIcon[] icons = new IIcon[subNames.length];
 	public static HashSet<String> bowSpeedPlayers = new HashSet<String>();
 
@@ -73,6 +73,8 @@ public class ItemMagicalBaubles extends Item implements IBauble, ITravellersGear
 		String type = getSlot(stack)>0?("tg."+getSlot(stack)):"bauble."+getBaubleType(stack);
 		list.add(StatCollector.translateToLocalFormatted(Lib.DESCRIPTION+"gearSlot."+type));
 
+		if(stack.hasTagCompound() && stack.getTagCompound().hasKey("title"))
+			list.add(StatCollector.translateToLocalFormatted(stack.getTagCompound().getString("title")));
 
 		if(Loader.isModLoaded("Botania"))
 		{
@@ -137,10 +139,23 @@ public class ItemMagicalBaubles extends Item implements IBauble, ITravellersGear
 	public void getSubItems(Item item, CreativeTabs tab, List itemList)
 	{
 		for(int i=0;i<subNames.length;i++)
-
+			if(i==4)
+			{
+				itemList.add(getItemWithTitle(new ItemStack(this,1,i),Lib.TITLE+"crimsonCultist"));
+				itemList.add(getItemWithTitle(new ItemStack(this,1,i),Lib.TITLE+"crimsonKnight"));
+				itemList.add(getItemWithTitle(new ItemStack(this,1,i),Lib.TITLE+"crimsonPraetor"));
+			}
+			else
 				itemList.add(new ItemStack(this,1,i));
 	}
 
+	public static ItemStack getItemWithTitle(ItemStack stack, String title)
+	{
+		if(!stack.hasTagCompound())
+			stack.setTagCompound(new NBTTagCompound());
+		stack.getTagCompound().setString("title", title);
+		return stack;
+	}
 
 	@Override
 	public boolean canEquip(ItemStack stack, EntityLivingBase living)

src/main/java/witchinggadgets/client/render/BlockRenderMetalDevice.java
@@ -19,7 +19,8 @@ import org.lwjgl.opengl.GL11;
 
 import thaumcraft.client.renderers.block.BlockRenderer;
 import witchinggadgets.common.blocks.tiles.TileEntityEssentiaPump;
-
+import witchinggadgets.common.blocks.tiles.TileEntityTerraformFocus;
+import witchinggadgets.common.blocks.tiles.TileEntityTerraformer;
 import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
 import cpw.mods.fml.client.registry.RenderingRegistry;
 
@@ -37,8 +38,18 @@ public class BlockRenderMetalDevice implements ISimpleBlockRenderingHandler
 			renderer.setRenderBoundsFromBlock(block);
 			BlockRenderer.drawFaces(renderer, block, block.getIcon(0, metadata), true);
 		}
-		else
-		{
+			else if(metadata==2) {
+				GL11.glTranslatef(-.5f, -.5F, -.5f);
+				TileEntityRendererDispatcher.instance.renderTileEntityAt(new TileEntityTerraformer(), 0.0D, 0.0D, 0.0D, 0.0F);
+		}
+			else if((metadata>=3&&metadata<=6)||metadata>7) {
+				GL11.glTranslatef(-.5f, -.5F, -.5f);
+				TileEntityTerraformFocus tetf = new TileEntityTerraformFocus();
+				tetf.blockType = block;
+				tetf.blockMetadata = metadata;
+				TileEntityRendererDispatcher.instance.renderTileEntityAt(tetf, 0.0D, 0.0D, 0.0D, 0.0F);
+			}
+			else {
 			GL11.glPushMatrix();
 			try{
 				if(metadata==0)

src/main/java/witchinggadgets/client/render/TileRenderTerraformer.java
@@ -1,0 +1,39 @@
+package witchinggadgets.client.render;
+
+import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.tileentity.TileEntity;
+
+import org.lwjgl.opengl.GL11;
+
+import thaumcraft.api.aspects.Aspect;
+import witchinggadgets.client.ClientProxy;
+import witchinggadgets.client.ClientUtilities;
+import witchinggadgets.common.blocks.tiles.TileEntityTerraformer;
+
+public class TileRenderTerraformer extends TileEntitySpecialRenderer
+{
+	public void renderTileEntityAt(TileEntity tile, double x, double y, double z, float f)
+	{
+		GL11.glPushMatrix();
+
+		//GL11.glDisable(GL11.GL_LIGHTING);
+		//		GL11.glDisable(GL12.GL_RESCALE_NORMAL);
+		GL11.glTranslatef((float)x+.5f, (float)y, (float)z+.5f);
+
+		if(ClientProxy.terraformerModel != null)
+		{
+			ClientUtilities.bindTexture("witchinggadgets:textures/models/terraformer.png");			ClientProxy.terraformerModel.renderPart("main_01");
+			if(tile!=null && tile.getWorldObj()!=null)
+			{
+				Aspect a = ((TileEntityTerraformer)tile).getSuctionType(null);
+				if(a!=null)
+					GL11.glColor3f((a.getColor()>>16&255)/255f, (a.getColor()>>8&255)/255f, (a.getColor()&255)/255f);
+			}
+			ClientProxy.terraformerModel.renderPart("crystals_02");
+			GL11.glColor3f(1, 1, 1);
+		}
+
+		GL11.glPopMatrix();
+	}
+
+}

src/main/java/witchinggadgets/common/WGConfig.java
@@ -14,7 +14,7 @@ public class WGConfig
 	public static boolean limitBookSearchToCategory,allowClusters,allowTransmutations,coremod_allowBootsRepair,
 	coremod_allowEnchantModifications,coremod_allowPotionApplicationMod,coremod_allowFocusPouchActive,
 	modulePrimal,moduleBag,bagVoid,bagEnder,bagHungry,moduleCloak,moduleKama,capeSpectral,capeStorage,capeWolf,
-	capeRaven,enableSearch,soulboundBaubles,soulboundGalacticraft,moduleGemcutting,allowdropsfrommachinery;
+	capeRaven,enableSearch,soulboundBaubles,soulboundGalacticraft,moduleGemcutting,allowdropsfrommachinery,terraformer;
 	
 	public static String[] tripplingClusterList;
 	public static int smelteryResultForClusters,cloakAnimationMode;
@@ -35,6 +35,7 @@ public class WGConfig
 		smelteryResultForClusters = config.get("Other Options", "Smeltery Result for Clusters", 144*3, "How many milliBuckets of molten Metal a cluster should give. 144mB equal 1 ingot. Set to 0 to disable smeltery recipes.").getInt();
 		allowClusters = config.get("Ore/Crucible", "Enable clusters", true, "Set this to false to disable clusters, useful when you are using AOBD.").getBoolean(true);
 		allowTransmutations = config.get("Ore/Crucible", "Enable transmutations", false, "Set this to false to disable nugget transmutations, this should fix the infinite loop glitch").getBoolean(false);
+		terraformer = config.getBoolean("Enable Terraformer", "Other Options", true, "If the Terraformer is enabled");
 		String[] clusters = {
 		//Tinkers
 		"Aluminum","Cobalt","Ardite",

src/main/java/witchinggadgets/common/WGContent.java
@@ -52,6 +52,8 @@ import witchinggadgets.common.blocks.tiles.TileEntitySaunaStove;
 import witchinggadgets.common.blocks.tiles.TileEntitySnowGen;
 import witchinggadgets.common.blocks.tiles.TileEntitySpinningWheel;
 import witchinggadgets.common.blocks.tiles.TileEntityTempLight;
+import witchinggadgets.common.blocks.tiles.TileEntityTerraformFocus;
+import witchinggadgets.common.blocks.tiles.TileEntityTerraformer;
 import witchinggadgets.common.blocks.tiles.TileEntityVoidWalkway;
 import witchinggadgets.common.blocks.tiles.TileEntityWallMirror;
 import witchinggadgets.common.items.EntityItemReforming;
@@ -271,8 +273,8 @@ public class WGContent
 		registerTile(TileEntityLabelLibrary.class);
 		//METAL
 		registerTile(TileEntityEssentiaPump.class);
-
-
+		registerTile(TileEntityTerraformer.class);
+		registerTile(TileEntityTerraformFocus.class);
 		//GameRegistry.registerTileEntity(TileEntityTotem.class, "TileEntityTotem");
 		//GameRegistry.registerTileEntity(TileEntityEssentiaVapourizer.class, "TileEntityEssentiaVapourizer");
 	}

src/main/java/witchinggadgets/common/WGResearch.java
@@ -170,6 +170,27 @@ public class WGResearch
 			
 		}
 		
+		if(WGConfig.terraformer) {
+		craftingAspects = new AspectList().add(Aspect.EARTH,10);
+		registerArcaneRecipe("TERRAFORMER","_PLAINS",new ItemStack(WGContent.BlockMetalDevice,1,3), craftingAspects, " S ","IBI","ITI", 'B',new ItemStack(Blocks.grass), 'I',"ingotIron", 'S',new ItemStack(ConfigItems.itemShard,1,6), 'T',new ItemStack(ConfigBlocks.blockTube));
+ 
+		craftingAspects = new AspectList().add(Aspect.WATER,10).add(Aspect.ORDER, 10);
+		registerArcaneRecipe("TERRAFORMFOCUS_COLDTAIGA","",new ItemStack(WGContent.BlockMetalDevice,1,4), craftingAspects, " S ","IBI","ITI", 'B',new ItemStack(Blocks.ice), 'I',"ingotIron", 'S',new ItemStack(ConfigItems.itemShard,1,6), 'T',new ItemStack(ConfigBlocks.blockTube));
+ 
+		craftingAspects = new AspectList().add(Aspect.FIRE,10).add(Aspect.EARTH, 10);
+		registerArcaneRecipe("TERRAFORMFOCUS_DESERT","",new ItemStack(WGContent.BlockMetalDevice,1,5), craftingAspects, " S ","IBI","ITI", 'B',new ItemStack(Blocks.sand), 'I',"ingotIron", 'S',new ItemStack(ConfigItems.itemShard,1,6), 'T',new ItemStack(ConfigBlocks.blockTube));
+ 
+		craftingAspects = new AspectList().add(Aspect.WATER,10).add(Aspect.EARTH, 10);
+		registerArcaneRecipe("TERRAFORMFOCUS_JUNGLE","",new ItemStack(WGContent.BlockMetalDevice,1,6), craftingAspects, " S ","IBI","ITI", 'B',new ItemStack(Blocks.log,1,3), 'I',"ingotIron", 'S',new ItemStack(ConfigItems.itemShard,1,6), 'T',new ItemStack(ConfigBlocks.blockTube));
+ 
+		craftingAspects = new AspectList().add(Aspect.FIRE,10).add(Aspect.ENTROPY, 10);
+		registerArcaneRecipe("TERRAFORMFOCUS_HELL","",new ItemStack(WGContent.BlockMetalDevice,1,7), craftingAspects, " S ","IBI","ITI", 'B',new ItemStack(Blocks.nether_brick), 'I',"ingotIron", 'S',new ItemStack(ConfigItems.itemShard,1,6), 'T',new ItemStack(ConfigBlocks.blockTube));
+		ThaumcraftApi.addWarpToItem(new ItemStack(WGContent.BlockMetalDevice,1,6), 1);
+ 
+		craftingAspects = new AspectList().add(Aspect.EARTH,10).add(Aspect.WATER, 10);
+		registerArcaneRecipe("TERRAFORMFOCUS_MUSHROOM","",new ItemStack(WGContent.BlockMetalDevice,1,8), craftingAspects, " S ","IBI","ITI", 'B',new ItemStack(Blocks.mycelium), 'I',"ingotIron", 'S',new ItemStack(ConfigItems.itemShard,1,6), 'T',new ItemStack(ConfigBlocks.blockTube));
+		}
+		
 		craftingAspects = new AspectList().add(Aspect.ENTROPY,5).add(Aspect.ORDER,5);
 		registerShapelessArcaneRecipe("ADVANCEDROBES","_CLOTH",new ItemStack(WGContent.ItemMaterial,1,5), craftingAspects, new ItemStack(WGContent.ItemMaterial,1,0), new ItemStack(WGContent.ItemMaterial,1,2),new ItemStack(WGContent.ItemMaterial,1,2), new ItemStack(WGContent.ItemMaterial,1,1));
 		
@@ -210,7 +231,14 @@ public class WGResearch
 		infusionAspects = new AspectList().add(Aspect.TRAVEL,4).add(Aspect.MIND, 6).add(Aspect.TOOL,2);
 		registerInfusionRecipe("LABYRINTHSTRING","",new ItemStack(WGContent.ItemMaterial,1,11),2,infusionAspects,new ItemStack(ConfigBlocks.blockMagicalLog,1,0),new ItemStack[] {new ItemStack(Items.ender_pearl),new ItemStack(WGContent.ItemMaterial,1,0),new ItemStack(WGContent.ItemMaterial,1,0),new ItemStack(WGContent.ItemMaterial,1,0)});
 
-
+		if(WGConfig.terraformer) {
+		infusionAspects = new AspectList().add(Aspect.ORDER, 16).add(Aspect.EXCHANGE, 8).add(Aspect.EARTH, 16);
+		registerInfusionRecipe("TERRAFORMER","",new ItemStack(WGContent.BlockMetalDevice,1,1),3,infusionAspects,new ItemStack(ConfigBlocks.blockMetalDevice,1,9),new ItemStack[] {new ItemStack(ConfigItems.itemShard,1,6), new ItemStack(Items.iron_ingot), new ItemStack(ConfigBlocks.blockTube), new ItemStack(ConfigBlocks.blockCustomPlant,1,1), new ItemStack(ConfigBlocks.blockTube), new ItemStack(Items.iron_ingot) });
+		
+		infusionAspects = new AspectList().add(Aspect.TAINT, 32).add(Aspect.EXCHANGE, 8);
+		registerInfusionRecipe("TERRAFORMFOCUS_TAINT","",new ItemStack(WGContent.BlockMetalDevice,1,8),3,infusionAspects,new ItemStack(ConfigBlocks.blockTaint,1,0),new ItemStack[] {new ItemStack(ConfigItems.itemShard,1,6),new ItemStack(Items.iron_ingot),new ItemStack(Items.iron_ingot),new ItemStack(ConfigBlocks.blockTube,1,0),new ItemStack(Items.iron_ingot),new ItemStack(Items.iron_ingot)});
+		ThaumcraftApi.addWarpToItem(new ItemStack(WGContent.BlockMetalDevice,1,8), 2);
+		}
 		
 		//Primordial Gear
 		if (WGConfig.modulePrimal) {
@@ -600,6 +628,7 @@ public class WGResearch
 		pages = new ResearchPage[]{ new ResearchPage("witchinggadgets_research_page.LABELLIB.1"), new ResearchPage((ShapedArcaneRecipe) recipeList.get("LABELLIB")) };
 		getResearchItem("LABELLIB", "WITCHGADG", researchAspects, -3, 1, 2, new ItemStack(WGContent.BlockWoodenDevice,1,5)).setParents("WG.JARLABEL").setPages(pages).setSecondary().registerResearchItem();
 
+		
 		
 		//MIRROR
 		if(Config.allowMirrors)

src/main/java/witchinggadgets/common/blocks/BlockWGMetalDevice.java
@@ -18,22 +18,23 @@ import net.minecraft.util.IIcon;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-
+import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraftforge.common.util.ForgeDirection;
 import thaumcraft.api.aspects.Aspect;
 import thaumcraft.common.config.ConfigBlocks;
 import thaumcraft.common.lib.world.ThaumcraftWorldGenerator;
 import witchinggadgets.WitchingGadgets;
-
+import witchinggadgets.api.ITerraformFocus;
 import witchinggadgets.client.render.BlockRenderMetalDevice;
 import witchinggadgets.common.blocks.tiles.TileEntityEssentiaPump;
-
+import witchinggadgets.common.blocks.tiles.TileEntityTerraformFocus;
+import witchinggadgets.common.blocks.tiles.TileEntityTerraformer;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class BlockWGMetalDevice extends BlockContainer 
+public class BlockWGMetalDevice extends BlockContainer implements ITerraformFocus
 {
-	public static String[] subNames = {"essentiaPump","voidmetalBlock"};
+	public static String[] subNames = {"essentiaPump","voidmetalBlock","terraformer","tfFocusPlains","tfFocusColdTaiga","tfFocusDesert","tfFocusJungle","tfFocusHell","tfFocusTaint","tfFocusMushroom"};
 	IIcon[] icons = new IIcon[subNames.length];
 
 	public BlockWGMetalDevice()
@@ -60,7 +61,7 @@ public class BlockWGMetalDevice extends BlockContainer
 	{	
 		for(int i=0;i<icons.length;i++)
 		{
-			if(i<1)
+			if(i==1)
 				icons[i] = iconRegister.registerIcon("thaumcraft:metalbase");
 			else
 				icons[i] = iconRegister.registerIcon("witchinggadgets:"+subNames[i]);
@@ -127,7 +128,9 @@ public class BlockWGMetalDevice extends BlockContainer
 			this.setBlockBounds(fd==ForgeDirection.EAST?.25f:0,fd==ForgeDirection.UP?.25f:0,fd==ForgeDirection.SOUTH?.25f:0, fd==ForgeDirection.WEST?.75f:1,fd==ForgeDirection.DOWN?.75f:1,fd==ForgeDirection.SOUTH?.75f:1);
 		}
 
-		else
+		else if (world.getBlockMetadata(x, y, z) >=2)
+			this.setBlockBounds(.125f,0,.125f, .875f,.75f,.875f);
+		else if (world.getBlockMetadata(x, y, z) > 0 && world.getBlockMetadata(x, y, z) < 2)
 			this.setBlockBounds(0,0,0,1,1,1);
 	}
 
@@ -140,8 +143,12 @@ public class BlockWGMetalDevice extends BlockContainer
 			return new TileEntityEssentiaPump();
 		case 1:
 			return null;
+		case 2:
+			return new TileEntityTerraformer();
+		default:
+			return new TileEntityTerraformFocus();
 		}
-		return null;
+		
 	}
 	@Override
 	public void getSubBlocks(Item item, CreativeTabs tab, List list)
@@ -175,6 +182,98 @@ public class BlockWGMetalDevice extends BlockContainer
 	public int getRenderType()
 	{
 		return BlockRenderMetalDevice.renderID;
+	}
+	@Override
+	public Aspect requiredAspect(World world, int x, int y, int z)
+	{
+		int meta = world.getBlockMetadata(x,y,z);
+		if(meta<subNames.length && subNames[meta].startsWith("tfFocus"))
+		{
+			if(subNames[meta].equalsIgnoreCase("tfFocusPlains"))
+				return Aspect.PLANT;
+			if(subNames[meta].equalsIgnoreCase("tfFocusColdTaiga"))
+				return Aspect.COLD;
+			if(subNames[meta].equalsIgnoreCase("tfFocusDesert"))
+				return Aspect.FIRE;
+			if(subNames[meta].equalsIgnoreCase("tfFocusJungle"))
+				return Aspect.TREE;
+			if(subNames[meta].equalsIgnoreCase("tfFocusHell"))
+				return Aspect.DARKNESS;
+			if(subNames[meta].equalsIgnoreCase("tfFocusTaint"))
+				return Aspect.TAINT;
+			if(subNames[meta].equalsIgnoreCase("tfFocusMushroom"))
+				return Aspect.SLIME;
+		}
+		return null;
+	}
+
+	@Override
+	public BiomeGenBase getCreatedBiome(World world, int x, int y, int z)
+	{
+		int meta = world.getBlockMetadata(x,y,z);
+		if(meta<subNames.length && subNames[meta].startsWith("tfFocus"))
+		{
+			if(subNames[meta].equalsIgnoreCase("tfFocusPlains"))
+				return BiomeGenBase.plains;
+			if(subNames[meta].equalsIgnoreCase("tfFocusColdTaiga"))
+				return BiomeGenBase.coldTaiga;
+			if(subNames[meta].equalsIgnoreCase("tfFocusDesert"))
+				return BiomeGenBase.desert;
+			if(subNames[meta].equalsIgnoreCase("tfFocusJungle"))
+				return BiomeGenBase.jungle;
+			if(subNames[meta].equalsIgnoreCase("tfFocusHell"))
+				return BiomeGenBase.hell;
+			if(subNames[meta].equalsIgnoreCase("tfFocusTaint"))
+				return ThaumcraftWorldGenerator.biomeTaint;
+			if(subNames[meta].equalsIgnoreCase("tfFocusMushroom"))
+				return BiomeGenBase.mushroomIsland;
+		}
+		return null;
+	}
+	@Override
+	public ItemStack getDisplayedBlock(World world, int x, int y, int z)
+	{
+		int meta = world.getBlockMetadata(x,y,z);
+		if(meta<subNames.length && subNames[meta].startsWith("tfFocus"))
+		{
+			if(subNames[meta].equalsIgnoreCase("tfFocusPlains"))
+				return new ItemStack(Blocks.grass);
+			if(subNames[meta].equalsIgnoreCase("tfFocusColdTaiga"))
+				return new ItemStack(Blocks.ice);
+			if(subNames[meta].equalsIgnoreCase("tfFocusDesert"))
+				return new ItemStack(Blocks.sand);
+			if(subNames[meta].equalsIgnoreCase("tfFocusJungle"))
+				return new ItemStack(Blocks.log,1,3);
+			if(subNames[meta].equalsIgnoreCase("tfFocusHell"))
+				return new ItemStack(Blocks.nether_brick);
+			if(subNames[meta].equalsIgnoreCase("tfFocusTaint"))
+				return new ItemStack(ConfigBlocks.blockTaint);
+			if(subNames[meta].equalsIgnoreCase("tfFocusMushroom"))
+				return new ItemStack(Blocks.mycelium);
+		}
+		return null;
+}
+
+	@Override
+	public Aspect requiredAspect(int meta) {
+		if(meta<subNames.length && subNames[meta].startsWith("tfFocus"))
+		{
+			if(subNames[meta].equalsIgnoreCase("tfFocusPlains"))
+				return Aspect.PLANT;
+			if(subNames[meta].equalsIgnoreCase("tfFocusColdTaiga"))
+				return Aspect.COLD;
+			if(subNames[meta].equalsIgnoreCase("tfFocusDesert"))
+				return Aspect.FIRE;
+			if(subNames[meta].equalsIgnoreCase("tfFocusJungle"))
+				return Aspect.TREE;
+			if(subNames[meta].equalsIgnoreCase("tfFocusHell"))
+				return Aspect.DARKNESS;
+			if(subNames[meta].equalsIgnoreCase("tfFocusTaint"))
+				return Aspect.TAINT;
+			if(subNames[meta].equalsIgnoreCase("tfFocusMushroom"))
+				return Aspect.SLIME;
+		}
+		return null;
 	}
 
 	

src/main/java/witchinggadgets/common/blocks/tiles/TileEntityTerraformFocus.java
@@ -1,0 +1,21 @@
+package witchinggadgets.common.blocks.tiles;
+
+import net.minecraft.nbt.NBTTagCompound;
+
+public class TileEntityTerraformFocus extends TileEntityWGBase
+{
+	@Override
+	public boolean canUpdate()
+	{
+		return false;
+	}
+
+	@Override
+	public void readCustomNBT(NBTTagCompound tags)
+	{
+	}
+	@Override
+	public void writeCustomNBT(NBTTagCompound tags)
+	{
+	}
+}

commit 4a0b3de1daf03145f86c241720aefd460d943bbc

Fix Crimson Title replacing Sniper Ring Bauble
Broken by https://github.com/DrParadox7/WitchingGadgets/commit/e25ca176dbc885b1c877c6df7fe1f5d5b9b730cd

src/main/java/witchinggadgets/client/ClientEventHandler.java
@@ -408,9 +408,9 @@ public class ClientEventHandler {
         IInventory baubles = BaublesApi.getBaubles(event.entity);
         if (Utilities.isPlayerUsingBow(event.entity) && baubles != null
                 && (OreDictionary
-                        .itemMatches(new ItemStack(WGContent.ItemMagicalBaubles, 1, 6), baubles.getStackInSlot(1), true)
+                        .itemMatches(new ItemStack(WGContent.ItemMagicalBaubles, 1, 5), baubles.getStackInSlot(1), true)
                         || OreDictionary.itemMatches(
-                                new ItemStack(WGContent.ItemMagicalBaubles, 1, 6),
+                                new ItemStack(WGContent.ItemMagicalBaubles, 1, 5),
                                 baubles.getStackInSlot(2),
                                 true))) {
             if (event.entity.isSneaking()) event.newfov = .25f;

src/main/java/witchinggadgets/common/items/baubles/ItemMagicalBaubles.java
@@ -39,7 +39,7 @@ public class ItemMagicalBaubles extends Item
 
     // String[] subNames = {"ringSocketed_gold","ringSocketed_thaumium","ringSocketed_silver"};
     public static String[] subNames = { "shouldersDoublejump", "shouldersKnockback", "vambraceStrength",
-            "vambraceHaste", "ringLuck", "titleCrimsonCult", "ringSniper" };
+            "vambraceHaste", "ringLuck", "ringSniper", "titleCrimsonCult" };
     IIcon[] icons = new IIcon[subNames.length];
     public static HashSet<String> bowSpeedPlayers = new HashSet<String>();
 
@@ -130,7 +130,7 @@ public class ItemMagicalBaubles extends Item
 
     @Override
     public void getSubItems(Item item, CreativeTabs tab, List itemList) {
-        for (int i = 0; i < subNames.length; i++) if (i == 4) {
+        for (int i = 0; i < subNames.length; i++) if (i == 5) {
             itemList.add(getItemWithTitle(new ItemStack(this, 1, i), Lib.TITLE + "crimsonCultist"));
             itemList.add(getItemWithTitle(new ItemStack(this, 1, i), Lib.TITLE + "crimsonKnight"));
             itemList.add(getItemWithTitle(new ItemStack(this, 1, i), Lib.TITLE + "crimsonPraetor"));